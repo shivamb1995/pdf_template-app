14f98877ec73ce1ec97447264e72a1d7afd41888
Shivam Baronia <shivambaronia007@gmail.com>
2023-05-23 15:43:40 +0530

    Added Pages

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ for index,row in df.iterrows():
     pdf.set_font(family='Times',style='B',size=24)
     pdf.cell(w=0,h=12,txt=row['Topic'],ln=1,align='L')
     pdf.line(x1=10,y1=21,x2=285,y2=21)
+    for i in range(row['Pages']-1):
+        pdf.add_page()
 pdf.output('Output.pdf')
